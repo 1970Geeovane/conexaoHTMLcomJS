561109116a34da23e8991b266bf682e6031c0e3c
Geovane <geovanesofiataborda@gmail.com>
2023-10-29 18:11:37 -0300

    Capitura de elementos

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-const nome = Geovane
-console.log(nome)
+let logo = document.getElementById("logo")
+//console.log(logo)
+
+let postAutor = document.getElementsByClassName("post-autor")
+//console.log(postAutor)
+
+let texto02 = document.querySelector("#post02 .post-texto")
+//console.log(texto02)
+
+let listaRedesFooter = document.querySelectorAll("footer .lista_redes")
+//console.log(listaRedesFooter)
+
+function percorrerArray(lista){
+    for(let i = 0; i< lista.length; i++){
+        console.log(lista[i]);
+    }
+}
+percorrerArray(listaRedesFooter)
